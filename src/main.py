e7bb298d2364cf11e0e370eb0a1231a1be0c5df3
Vladimir Bogdanov <104579604+VIBogdanov@users.noreply.github.com>
2024-03-06 13:23:14 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
 from assistools import get_ranges_index
+from puzzles import get_combination_numbers, get_number_permutations
 from sundry import (
     find_intervals,
     find_item_by_binary,
     find_item_by_interpolation,
+    find_nearest_number,
     get_common_divisor,
+    sort_by_bubble,
+    sort_by_merge,
+    sort_by_merge2,
+    sort_by_selection,
+    sort_by_shell,
 )
 
 if __name__ == "__main__":
@@ -37,6 +44,42 @@ if __name__ == "__main__":
     for res in find_intervals([1, -1, 4, 3, 2, 1, -3, 4, 5, -5, 5], 0):
         print(tuple(res), end=" ")
     print("")
+
+    print(
+        "\n- Поиск ближайшего целого числа, которое меньше или больше заданного и состоит из тех же цифр."
+    )
+    print(f" find_nearest_number(273145) -> {find_nearest_number(273145)}")
+
+    print("\n- Сортировки методом пузырька.")
+    print(
+        f" sort_by_bubble([2, 7, 3, 1, 4, 5]) -> {sort_by_bubble([2, 7, 3, 1, 4, 5])}"
+    )
+
+    print("\n- Сортировки методом слияния.")
+    print(f" sort_by_merge([2, 7, 3, 1, 4, 5]) -> {sort_by_merge([2, 7, 3, 1, 4, 5])}")
+
+    print("\n- Усовершенствованная версия сортировки методом слияния.")
+    print(
+        f" sort_by_merge2([2, 7, 3, 1, 4, 5]) -> {sort_by_merge2([2, 7, 3, 1, 4, 5])}"
+    )
+
+    print("\n- Сортировки методом Shell.")
+    print(f" sort_by_shell([2, 7, 3, 1, 4, 5]) -> {sort_by_shell([2, 7, 3, 1, 4, 5])}")
+
+    print("\n- Сортировки методом выбора.")
+    print(
+        f" sort_by_selection([2, 7, 3, 1, 4, 5]) -> {sort_by_selection([2, 7, 3, 1, 4, 5])}"
+    )
+
+    print(
+        "\n- Минимальное количество перестановок, которое необходимо произвести для выравнивания списков."
+    )
+    print(
+        f" get_number_permutations([10, 31, 15, 22, 14, 17, 16], [16, 22, 14, 10, 31, 15, 17])) -> {get_number_permutations([10, 31, 15, 22, 14, 17, 16], [16, 22, 14, 10, 31, 15, 17])}"
+    )
+
+    print("\n- Сформировать все возможные уникальные наборы чисел из указанных цифр.")
+    print(f" get_combination_numbers([2, 7]) -> {get_combination_numbers([2, 7])}")
 
     print("")
     pass
